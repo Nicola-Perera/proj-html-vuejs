8564112859c30273f1d19d0eff671b009791d46a
Nicola-Perera <nicola.perera96@mail.com>
2021-11-25 12:17:02 +0100

    site header initial comit

--- src/App.vue
+++ src/App.vue
@@ -27,10 +27,5 @@ export default {
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/header/SiteHeader.vue
+++ src/components/header/SiteHeader.vue
@@ -1,5 +1,19 @@
 <template>
-    <div id="SiteHeader"></div>
+    <div id="SiteHeader">
+
+        <!-- general contacts -->
+        <div class="header_top">
+            <div class="main_container">
+                <a href="#">Contact us</a>
+            </div>
+            <div id="header_logo">
+                <img src="../../assets/img/source_images/logo-default-slim.png" alt="">
+            </div>
+        </div>
+        <div class="header_bottom">
+            <a href="#">Home</a>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,6 +26,13 @@ export default {
 
 </script>
 
-<style lang="scss">
+<style>
+img {
+    width: 5rem;
+    }
+
+.header_bottom {
+    border-top: 2px solid #ededed;
+}
 
 </style>
