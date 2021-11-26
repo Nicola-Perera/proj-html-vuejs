ae2dfd6a0ca749e21f1ec0fa5a00cd93058b0f28
Nicola-Perera <nicola.perera96@mail.com>
2021-11-26 14:42:58 +0100

    add recent posts content to the murkup

--- src/components/main/MiniPost.vue
+++ src/components/main/MiniPost.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="MiniPost">
-      <h3>{{ post.genre }}</h3>
+      <div class="container">
+        <div class="post_image">
+          <img :src="require('../../assets/img/source_images/' + post.image + '.jpg')" alt="">
+        </div>
+        <div class="post_content">
+          <h3>{{ post.title }}</h3>
+          <h4><span>{{ post.date.month }}</span> <span>{{ post.date.day }}</span>, <span>{{ post.date.year }}</span></h4>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -15,5 +23,14 @@ export default {
 
 <style lang="scss">
 @import '../../assets/style/common.scss';
+
+#MiniPost {
+  .post_image {
+    width: 3rem;
+    img {
+      width: 100%;
+    }
+  }
+}
 
 </style>
