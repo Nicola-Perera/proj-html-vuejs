03a1b755e260da4a530bf3b3d7bd2e381633c012
Nicola-Perera <nicola.perera96@mail.com>
2021-11-29 17:06:28 +0100

    add "about blog" block to the Footer

--- src/components/footer/SiteFooter.vue
+++ src/components/footer/SiteFooter.vue
@@ -1,5 +1,31 @@
 <template>
-    <div class="SiteFooter"></div>
+    <div class="SiteFooter">
+      <div class="width80 container">
+
+        <!-- info about the blog -->
+        <div class="about">
+          <h3>ABOUT THE BLOG</h3>
+          <p>Lorem ipsum sit amet, consectetur adipiscing elit. Donec eu pulvinar magna semper scelerisque.</p>
+          <p>Proesent venenatis turpis vitae purus semper, eget sogittis velit venenatis ptent taciti sociosqu ad litora...</p>
+          <h3>VIEW MORE &rArr;</h3>
+        </div>
+
+        <!-- most recent posts -->
+        <div class="most_recent_posts">
+          <h3>RECENT POSTS</h3>
+        </div>
+
+        <!-- most recent comments -->
+        <div class="most_recent_comments">
+          <h3>RECENT Comments</h3>
+        </div>
+
+        <!-- categories -->
+        <div class="categories">
+          <h3>CATEGORIES</h3>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -16,5 +42,25 @@ export default {
 
 <style lang="scss">
 @import '../../assets/style/common.scss';
+
+.SiteFooter {
+  background-color: var(--theme-dark-grey);
+  font-size: .6rem;
+  .width80 {
+    padding-top: 4rem;
+    padding-bottom: 6rem;
+  }
+  h3 {
+    color: white;
+  }
+  p {
+    color: #888888;
+    margin-top: 1rem;
+    margin-bottom: .6rem;
+  }
+  .about, .most_recent_posts, .most_recent_comments, .categories {
+    width: calc(100% / 4);
+  }
+}
 
 </style>
